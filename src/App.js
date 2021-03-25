4d39872892d5d37766dced5bf712b68246cef40a
mlvrkhn <martin.guide@gmail.com>
2021-03-25 17:48:22 +0100

    Add Prismic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import Prismic from '@prismicio/client';
+import { Date, Link, RichText } from 'prismic-reactjs';
 
 const App = () => {
+console.log('🚀 ~ Prismic', Prismic)
     return (
         <div>
             BLOG
